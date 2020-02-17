91f8b11413df467fb7f51a1ddb886a383291927c
ScandalAnimal <mvasilisin@gmail.com>
2020-02-17 23:11:38 +0100

    updated hooks example

--- src/components/APIHookTest.jsx
+++ src/components/APIHookTest.jsx
@@ -1,27 +1,44 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
+import React, {useState, Fragment} from 'react';
+import {useHNApi} from '../hooks/useHNApi';
 
 function APIHookTest() {
-  const [data, setData] = useState({hits: []});
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios(
-        'https://hn.algolia.com/api/v1/search?query=redux'
-      );
-      setData(result.data);
-    };
-    fetchData();
-  }, []);
+  const [query, setQuery] = useState('redux');
+  const [
+    {data, isLoading, isError},
+    doFetch
+  ] = useHNApi('http://hn.algolia.com/api/v1/search?query=redux', {hits: []});
 
   return (
-    <ul>
-      {data.hits.map(item => (
-        <li key={item.objectID}>
-          <a href={item.url}>{item.title}</a>
-        </li>
-      ))}
-    </ul>
+    <Fragment>
+      <form
+        onSubmit={event => {
+          doFetch(`http://hn.algolia.com/api/v1/search?query=${query}`);
+
+          event.preventDefault();
+        }}
+      >
+        <input
+          type="text"
+          value={query}
+          onChange={event => setQuery(event.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+
+      {isError && <div>Something went wrong ...</div>}
+
+      {isLoading ? (
+        <div>Loading ...</div>
+      ) : (
+        <ul>
+          {data.hits.map(item => (
+            <li key={item.objectID}>
+              <a href={item.url}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+      )}
+    </Fragment>
   );
 }
 export default APIHookTest;
