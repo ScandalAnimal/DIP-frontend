cc5ace5f8e277b8121ad593b9271d923c78e59df
ScandalAnimal <mvasilisin@gmail.com>
2020-03-18 23:27:10 +0100

    added icons in fixtures

--- src/components/Fixtures/FixtureTable.js
+++ src/components/Fixtures/FixtureTable.js
@@ -2,15 +2,28 @@ import { mockFixtures } from '../../mocks/mockFixtures';
 import { mockTeams } from '../../mocks/mockTeams';
 import Button from '../Button/Button';
 import React from 'react';
+import teamLogoService from '../../service/teamLogoService';
+
+function renderTeamName(teams, id, isHome) {
+  const className = isHome ? 'd-flex flex-row' : 'd-flex flex-row-reverse';
+  return (
+    <div className={className}>
+      <div className='fixture-team-icon'>
+        <img src={teamLogoService.getTeamLogo(id)} alt='Team logo' width={25} />
+      </div>
+      <div className='fixture-team-name'>{teams[id].name}</div>
+    </div>
+  );
+}
 
 function renderFixture(fixture) {
   const teams = mockTeams; // TODO replace with API call
 
   return (
     <div className='fixture-wrapper row'>
-      <div className='col-xl-4 text-left'>{teams[fixture.home].name}</div>
+      <div className='col-xl-4 text-left'>{renderTeamName(teams, fixture.home, true)}</div>
       <div className='col-xl-4 text-center'> - </div>
-      <div className='col-xl-4 text-right'>{teams[fixture.away].name}</div>
+      <div className='col-xl-4 text-right'>{renderTeamName(teams, fixture.away, false)}</div>
     </div>
   );
 }
