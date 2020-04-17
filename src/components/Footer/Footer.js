a524b62cddef91ad7263fc2935d0b806bfdba183
ScandalAnimal <mvasilisin@gmail.com>
2020-04-17 03:21:35 +0200

    added link to profile

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,10 @@ const Footer = () => {
     <div className='d-flex flex-column'>
       <footer className='footer footer-bottom'>
         <div className='ml-auto'>
-          <div>Made by Maros</div>
+          <div>
+            Made by{' '}
+            <a href='https://www.linkedin.com/in/maro%C5%A1-vasili%C5%A1in-5202b9107/'>Maros</a>
+          </div>
         </div>
       </footer>
     </div>
